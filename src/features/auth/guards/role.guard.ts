1d9b70eb5d963133334082f4db60e023a1e9b594
henrychris <64085210+henrychris@users.noreply.github.com>
2024-10-19 22:32:28 +0100

    create role guard

---
+++ src/features/auth/guards/role.guard.ts
@@ -0,0 +1,38 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from 'src/common/enums/role.enum';
+import { ROLES_KEY } from 'src/decorators/roles.decorator';
+import { User } from 'src/features/users/entities/user.entity';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  async canActivate(context: ExecutionContext) {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) {
+      return true;
+    }
+
+    let request = context.switchToHttp().getRequest();
+    const user = request.user as User;
+    if (!user) {
+      throw new UnauthorizedException('User not authenticated');
+    }
+
+    console.log(request.user);
+    console.log(JSON.stringify(user));
+
+    return requiredRoles.some((role) =>
+      user.roles?.find((x) => x.name === role),
+    );
+  }
+}
