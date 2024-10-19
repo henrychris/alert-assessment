b7885252b71d2e8f7b063986b45605947c6ca255
henrychris <64085210+henrychris@users.noreply.github.com>
2024-10-19 22:32:39 +0100

    use auth & role guards

--- src/features/users/users.controller.ts
+++ src/features/users/users.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from 'src/common/enums/role.enum';
+import { RolesGuard } from '../auth/guards/role.guard';
+import { AuthGuard } from '../auth/guards/auth.guard';
 
 @Controller('users')
+@UseGuards(AuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
@@ -11,6 +16,8 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @Roles(Role.Admin)
+  @UseGuards(RolesGuard)
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
