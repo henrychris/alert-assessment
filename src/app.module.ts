eaa052e0b60767b63a00bcae9505deda8d134acf
henrychris <64085210+henrychris@users.noreply.github.com>
2024-10-19 20:37:53 +0100

    update config setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,11 +21,7 @@ import * as Joi from 'joi';
           .valid('development', 'production', 'test')
           .default('development'),
         PORT: Joi.number().required(),
-        DATABASE_HOST: Joi.string().required(),
-        DATABASE_PORT: Joi.string().required(),
-        DATABASE_NAME: Joi.string().required(),
-        DATABASE_USERNAME: Joi.string().required(),
-        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_URL: Joi.string().required(),
         JWT_SECRET: Joi.string().required(),
       }),
     }),
