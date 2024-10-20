2ee9bca37e7794a67dfbcfbe3be61d301a266a37
henrychris <64085210+henrychris@users.noreply.github.com>
2024-10-20 15:45:09 +0100

    mock providers for seed service

--- src/features/seed/seed.service.spec.ts
+++ src/features/seed/seed.service.spec.ts
@@ -1,15 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SeedService } from './seed.service';
+import { PrismaService } from '../../prisma/prisma.service';
 
 describe('SeedService', () => {
   let service: SeedService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SeedService],
+      providers: [
+        SeedService,
+        {
+          provide: PrismaService,
+          useValue: {
+            user: {
+              findMany: jest.fn(),
+              findUnique: jest.fn(),
+              findFirst: jest.fn(),
+              create: jest.fn(),
+              delete: jest.fn(),
+              update: jest.fn(),
+            },
+            role: {
+              findFirst: jest.fn(),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<SeedService>(SeedService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
